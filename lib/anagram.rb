0ff11b5a4a03c0d4e7d6fc1c546323ede17e677c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-11 18:00:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,23 @@
 # Your code goes here!
+require 'pry'
+
 class Anagram
 
-attr_accessor :word
+  attr_accessor :word
+
+
 
 def initialize(word)
   @word = word
 end
 
+
+
 def match(array_of_words)
-  array_of_words.select do |annagram| @word.split("").sort == annagram.split("").sort
+  array_of_words.select do |element|
+    (@word.split("").sort) == (element.split("").sort)
+  end
 end
-end
+
 
 end
